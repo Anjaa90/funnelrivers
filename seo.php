8a781ba8155caf3b8d4189bff48b35fd8f6a447b
Anjaa90 <anjali.prixim@gmail.com>
2025-01-03 17:48:04 +0530

    careerandtestimonial page

--- seo.php
+++ seo.php
@@ -5,50 +5,7 @@
 <!-- End funnelRivers Main Menu Area -->
 <!--==================================================-->
 <!-- Sidebar Cart Item -->
-<div class="xs-sidebar-group info-group">
-	<div class="xs-overlay xs-bg-black"></div>
-	<div class="xs-sidebar-widget">
-		<div class="sidebar-widget-container">
-			<div class="widget-heading">
-				<a href="#" class="close-side-widget">
-					<i class="far fa-times-circle"></i>
-				</a>
-			</div>
-			<div class="sidebar-textwidget">
-				
-				<div class="sidebar-info-contents">
-					<div class="content-inner">
-						<div class="nav-logo">
-							<a href="index.html"><img src="assets/images/logo.png" alt="" /></a>
-						</div>
-						<div class="content-box">
-							<h2>About Us</h2>
-							<p class="text">The argument in favor of using filler text goes something like this: If you use real content in the Consulting Process, anytime you reach a review point you’ll end up reviewing and negotiating the content itself and not the design.</p>
-							<a href="index-7.html" class="theme-btn btn-style-two"><span>Consultation</span> <i class="fas fa-heart"></i></a>
-						</div>
-						<div class="contact-info">
-							<h2>Contact Info</h2>
-							<ul class="list-style-one">
-								<li><span class="icon flaticon-email"></span>Chicago 12, Melborne City, USA</li>
-								<li><span class="flaticon-phone-call"></span>(+001) 123-456-7890</li>
-								<li><span class="icon flaticon-placeholder-1"></span>Example.com</li>
-								<li><span class="flaticon-clock-2"></span>Week Days: 09.00 to 18.00 Sunday: Closed</li>
-							</ul>
-						</div>
-					
-						<ul class="social-box">
-							<li class="facebook"><a href="#" class="fab fa-facebook-f"></a></li>
-							<li class="twitter"><a href="#" class="fab fa-instagram"></a></li>
-							<li class="linkedin"><a href="#" class="fab fa-twitter"></a></li>
-							<li class="instagram"><a href="#" class="fab fa-pinterest-p"></a></li>
-							<li class="youtube"><a href="#" class="fab fa-linkedin-in"></a></li>
-						</ul>
-					</div>
-				</div>		
-			</div>
-		</div>
-	</div>
-</div>
+
 <!--==================================================-->
 <!-- start funnelRivers slider Area -->
 <!--==================================================-->
