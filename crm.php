5cb641755c9502a24180aec658b55b02eb315c94
Avinashkdm10 <avinash.prixim@gmail.com>
2025-02-06 14:41:53 +0530

    commit

--- crm.php
+++ crm.php
@@ -28,7 +28,7 @@
 
 
 <!--==================================================-->
-<!-- Start dreamit service Area -->
+<!-- Start funnelriverss service Area -->
 <!--==================================================-->
 <div class="service-area" id="blog">
 	<div class="container">		
@@ -36,21 +36,28 @@
 			<div class="col-lg-8 col-md-6 col-sm-12 col-sm-12">
 				<div class="row">
 					<div class="col-lg-12 col-sm-12">
-						<div class="dreamit-single-service-box">
-							<div class="dreamit-sigle-tmumb">
+						<div class="funnelriverss-single-service-box">
+							<div class="funnelriverss-sigle-tmumb">
 								<img loading="lazy" src="assets/images/resource/img-1.jpg" alt="">
 							</div>
-							
+							<div class="em-service-content">
+								<div class="service-page-title">
+									<h1><a href="#">funnelrivers Website Development Services
+									</a></h1>
+									<p>funnelrivers brings your brand to life with powerful Website Development that drive engagement and conversions. Our team creates eye-catching video ads tailored to resonate with your target audience. Using YouTube’s advanced targeting tools, we ensure your ads reach the right viewers at the right moment, boosting your brand’s visibility and impact. We continuously track campaign performance, optimize based on data, and refine strategies to maximize your return on investment. With funnelrivers’s Website Development, your business will experience enhanced brand awareness and measurable results. Let’s elevate your online presence and achieve your goals with the power of video advertising!
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
 						<div class="service-details-box">
 							<div class="service-details-icon">
-							<i class="fas fa-cogs"></i> 
+								<i class="fas fa-award"></i>
 							</div>
 							<div class="service-details-title">
-								<h4>Tailored Content Strategy</h4>
-								<p>Developing a custom content strategy based on your brand’s goals and target audience. <br><br>
+								<h4>Expert Content Creation</h4>
+								<p>High-quality, engaging content designed to resonate with your audience.
 								</p>
 							</div>
 						</div>
@@ -58,78 +65,54 @@
 					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
 						<div class="service-details-box">
 							<div class="service-details-icon">
-							<i class="fas fa-pencil-alt"></i>
+								<i class="flaticon-support-6"></i>
 							</div>
 							<div class="service-details-title">
-								<h4>Engaging Blog Posts and Articles
-								</h4>
-								<p> Writing high-quality, SEO-optimized blog posts and articles to inform, engage, and drive traffic.
+								<h4>Performance Monitoring</h4>
+								<p> Regular analytics and optimization to improve results.
 								</p>
 							</div>
 						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
-						<div class="service-details-box">
-							<div class="service-details-icon">
-							<i class="fas fa-share-alt"></i>
-							</div>
-							<div class="service-details-title">
-								<h4>Social Media Content
-								</h4>
-								<p> Crafting shareable and engaging content for platforms like Facebook, Instagram, Twitter, and LinkedIn.
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
-						<div class="service-details-box">
-							<div class="service-details-icon">
-							<i class="fas fa-video"></i> 
-							</div>
-							<div class="service-details-title">
-								<h4>Video Content Writing
-								</h4>
-								<p>Producing creative and engaging videos for marketing campaigns, social media, and websites. <br><br>
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
-						<div class="service-details-box">
-							<div class="service-details-icon">
-							<i class="fas fa-image"></i>
-							</div>
-							<div class="service-details-title">
-								<h4>Infographics and Visual Content
-								</h4>
-								<p>Designing eye-catching infographics, presentations, and visuals to simplify complex information.
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 srvce-upper pt-20 pb-50">
-						<div class="service-details-box">
-							<div class="service-details-icon">
-							<i class="fas fa-search"></i>
-							</div>
-							<div class="service-details-title">
-								<h4>SEO-Optimized Content
-								</h4>
-								<p>Writing content that’s optimized for search engines to drive organic traffic and improve rankings. <br><br>
-								</p>
-							</div>
-						</div>
-					</div>
-					
-					<div class="col-lg-6 col-md-6 col-sm-12">
-						
 					</div>
 					<div class="col-lg-6 col-md-6 col-sm-12">
-						
+						<div class="service-details-thumb">
+							<img loading="lazy" src="assets/images/resource/img-2.png" alt="">
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-12">
+						<div class="service-details-icon-box">
+							<div class="service-details-icon-title">
+								<h3>We Serve the Best Work in Website Development!
+								</h3>
+								<p>We deliver exceptional website development with tailored strategies, targeted solutions.</p>
+							</div>
+						</div>
+						<div class="widget-service-details-icon">
+							<p><i class="fas fa-check"></i> <span>Tailored Strategies</span></p>
+							<p><i class="fas fa-check"></i> <span>Targeted Ads</span></p>
+							<p><i class="fas fa-check"></i> <span>Data-Driven Results </span></p>
+						</div>
 					</div>
 					<div class="col-lg-12 col-sm-12">
 					<div class="widget-details-text pt-25 pb-3">
-    					
+    					<p><i class="fas fa-check"></i> <b>Custom Web Design and Development</b> 
+    					Crafting visually appealing, responsive websites tailored to your brand’s needs for all devices.
+    					</p>
+    					<p><i class="fas fa-check"></i> <b>E-Commerce Website Solutions</b>
+    					Building secure, user-friendly e-commerce sites with payment integration and smooth checkout.
+    					</p>
+    					<p><i class="fas fa-check"></i> <b>Content Management Systems (CMS)</b> 
+    					Developing CMS-based websites for easy content management with platforms like WordPress.
+    					</p>
+    					<p><i class="fas fa-check"></i> <b>SEO-Optimized Web Development</b>
+    					Creating SEO-friendly websites with fast load times and mobile responsiveness for better search rankings.
+    					</p>
+    					<p><i class="fas fa-check"></i> <b>Web Application Development</b>
+    					Developing custom web apps for enhanced functionality, automation, and user engagement.
+    					</p>
+    					<p><i class="fas fa-check"></i> <b>UI/UX Design</b>
+    					Designing intuitive and user-centered websites for a seamless and engaging user experience.
+    					</p>
 					</div>
 
 					</div>
@@ -150,7 +133,7 @@
 				</div>
 				<div class="widget-service-thumb-box">
 					<div class="wedget-service-thumb">
-						<img loading="lazy" src="assets\images\Creations\content-writing-1-image-370x500.png" alt="content-writing">		
+						<img loading="lazy" src="assets\images\Creations\website-development-1-image-370x500.png" alt="">		
 						<!-- <div class="widget-service-content text-center">
 							<div class="widget-text">
 								<p>Contact Us Now</p>
@@ -169,14 +152,12 @@
 						</div> -->
 					</div>
 				</div>
-				
 			</div>	
 		</div>
 	</div>
 </div>
 <!--==================================================-->
-<!-- End dreamit service Area -->
-<!--==================================================-->
+<!-- End funnelriverss service Area -->
 
 
 
